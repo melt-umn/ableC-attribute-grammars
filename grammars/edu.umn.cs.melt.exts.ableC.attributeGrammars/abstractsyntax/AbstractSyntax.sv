2644af14f45138795663c5cae353f192d83f2e00
Lucas Kramer <krame505@umn.edu>
2019-12-01 18:45:24 -0600

    Initial commit

---
+++ grammars/edu.umn.cs.melt.exts.ableC.attributeGrammars/abstractsyntax/AbstractSyntax.sv
@@ -0,0 +1,14 @@
+grammar edu:umn:cs:melt:exts:ableC:attributeGrammars:abstractsyntax;
+
+imports silver:langutil; 
+imports silver:langutil:pp;
+
+imports edu:umn:cs:melt:ableC:abstractsyntax:host;
+imports edu:umn:cs:melt:ableC:abstractsyntax:overloadable;
+imports edu:umn:cs:melt:ableC:abstractsyntax:construction;
+imports edu:umn:cs:melt:ableC:abstractsyntax:env;
+
+imports edu:umn:cs:melt:exts:ableC:algebraicDataTypes:datatype:abstractsyntax;
+
+global builtin::Location = builtinLoc("attributeGrammars");
+
---
+++ grammars/edu.umn.cs.melt.exts.ableC.attributeGrammars/concretesyntax/ConcreteSyntax.sv
@@ -0,0 +1,6 @@
+grammar edu:umn:cs:melt:exts:ableC:attributeGrammars:concretesyntax;
+
+imports edu:umn:cs:melt:ableC:concretesyntax;
+imports silver:langutil only ast; 
+
+imports edu:umn:cs:melt:exts:ableC:attributeGrammars:abstractsyntax;
